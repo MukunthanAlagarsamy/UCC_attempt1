03fc3db21e3042b457c5aa3a1d2f3250b7b5e1c0
MukunthanAlagarsamy <mukunthan.logan@gmail.com>
2017-01-30 00:43:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def makeWebhookResult(req):
     parameters = result.get("parameters")
     zone = parameters.get("shipping-zone")
 
-    cost = {'Europe':'Europe is selected', 'North America':'NA is selected', 'South America':'SA is selected', 'Asia':'Asia is selected', 'Africa':'Africa is selected'}
+    cost = {'Europe':'Europe is selected', 'North America':'North is selected', 'South America':'SA is selected', 'Asia':'Asia is selected', 'Africa':'Africa is selected'}
 
     speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
 
